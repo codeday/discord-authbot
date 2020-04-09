b0a50c5d3f53d4adf6f841757e11cd313cf5a280
DragonBallZeke <zeke@egherman.com>
2020-04-09 14:03:27 -0500

    pretty print result

--- src/cogs/auth.py
+++ src/cogs/auth.py
@@ -1,4 +1,5 @@
 import os
+import json
 from utils.auth0 import lookup_user
 from utils.person import id_from_mention
 
@@ -15,11 +16,12 @@ class AuthCommands(commands.Cog, name="Authentication"):
     @commands.command(name='check-clear', hidden=True)
     @commands.has_any_role('Global Staff')
     async def check_clear(self, ctx, user):
+        """Lookup a discord users clear account"""
         user = id_from_mention(user)
         results = lookup_user(user)
         if len(results) == 1:
             user = results[0]
-            await ctx.send(str(user))
+            await ctx.send(json.dumps(user, indent=2, sort_keys=True))
         else:
             await ctx.send('')
 
