963c2c5057cfa72e169829034e216bb1009ebae0
DragonBallZeke <zeke@egherman.com>
2020-05-14 21:04:46 -0500

    fix jake dum dum

--- src/utils/SuperBot.py
+++ src/utils/SuperBot.py
@@ -13,6 +13,6 @@ class SuperBot(commands.Bot):
     def __init__(self, command_prefix, help_command=_default, description=None, **options):
         super(SuperBot, self).__init__(command_prefix, help_command, description, **options)
 
-    def process_commands(self, message):
+    async def process_commands(self, message):
         ctx = await self.get_context(message)
         await self.invoke(ctx)
