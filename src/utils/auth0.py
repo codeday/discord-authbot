657dbbc25a6a1cdaad2461ae3ce47caadf713c07
DragonBallZeke <zeke@egherman.com>
2020-05-26 13:29:10 -0500

    add lookup_all function for finding all members with linked discord

--- src/utils/auth0.py
+++ src/utils/auth0.py
@@ -1,7 +1,7 @@
 import os
 
-from auth0.v3.management import Auth0
 from auth0.v3.authentication import GetToken
+from auth0.v3.management import Auth0
 
 
 def lookup_user(user: int,
@@ -14,3 +14,14 @@ def lookup_user(user: int,
                                          'https://{}/api/v2/'.format(domain))['access_token']
     mgmt = Auth0(domain, token)
     return mgmt.users.list(q=f'user_metadata.discord_id:"{str(user)}"')['users']
+
+
+def lookup_all(domain=os.getenv('AUTH_DOMAIN'),
+               client_id=os.getenv('AUTH_CLIENT_ID'),
+               client_secret=os.getenv('AUTH_CLIENT_SECRET')):
+    get_token = GetToken(domain)
+    token = get_token.client_credentials(client_id,
+                                         client_secret,
+                                         'https://{}/api/v2/'.format(domain))['access_token']
+    mgmt = Auth0(domain, token)
+    return mgmt.users.list(q='user_metadata.discord_id')['users']
