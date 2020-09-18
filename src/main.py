98c65a2ce0fc2503f44f5fa3e4e93fddb70bc163
Lola <lola@egherman.com>
2020-09-18 18:08:16 -0500

    implement support functionality and message send confirmation

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import sys
 import traceback
 from os import getenv
 
+import discord
+from discord import MessageType
 from raygun4py import raygunprovider
 
 from utils.SuperBot import SuperBot
 from utils.auth0 import lookup_user
 
 logging.basicConfig(level=logging.INFO)
+welcome_channel_id = getenv('WELCOME_CHANNEL_ID', '756583187307823224')
 
 
 def handle_exception(exc_type, exc_value, exc_traceback):
@@ -44,7 +47,20 @@ async def on_member_join(member):
             '''Hello, human! Welcome to the CodeDay Discord server!
 To gain full access, you MUST link your Discord account to a CodeDay account using the link below:
 https://discord0.codeday.org
-''')
 
+If you need any support with this process, reply to this message and a staff member will be in touch shortly.
+''')
+    welcome_channel = bot.get_channel(int(welcome_channel_id))
+    last_message = welcome_channel.fetch_message(welcome_channel.last_message_id)
+    if last_message.type == MessageType.new_member and last_message.author == member:
+        last_message.add_reaction('👋')
+
+
+@bot.event
+async def on_message(message):
+    if type(message.channel) == discord.channel.DMChannel and message.author is not message.channel.me:
+        welcome_channel = bot.get_channel(int(welcome_channel_id))
+        welcome_channel.send_message(f'''<@{message.author.id}> just messaged bot:
+{message.content}''')
 
 bot.run(BOT_TOKEN, bot=True, reconnect=True)
