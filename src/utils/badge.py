6a78d74604a0660c7137161d19c14435f444be9c
Lola <lola@egherman.com>
2020-09-24 18:49:44 -0500

    add some more functions

--- src/utils/badge.py
+++ src/utils/badge.py
@@ -49,3 +49,57 @@ def get_badges_by_discord_id(discord_id):
     badges = json.loads(r.text)['data']['account']['getUser']['badges']
     return badges
 
+
+def get_badges_by_username(username):
+    """returns a list of json objects containing badge details"""
+    query = '''
+    {
+      account {
+        getUser(where: {username: "''' + str(username) + '''"}, fresh: true) {
+          badges {
+            details {
+              id,
+              name,
+              description,
+              emoji
+            }
+          }
+        }
+      }
+    }
+    '''
+    r = requests.post(graphql_url, json={'query': query})
+    badges = json.loads(r.text)['data']['account']['getUser']['badges']
+    return badges
+
+
+def username_from_discord_id(discord_id):
+    query = '''
+    {
+      account {
+        getUser(where: {discordId: "''' + discord_id + '''"}, fresh: true) {
+          username 
+          }
+        }
+      }
+    '''
+    r = requests.post(graphql_url, json={'query': query})
+    username = json.loads(r.text)['data']['account']['getUser']['username']
+    return username
+
+
+def grant_badge_by_username(badge_id, username, token):
+    query = '''
+    mutation {
+      account {
+        grantBadge(username:"''' + username + '''", badge:{id: "''' + badge_id + '''"})
+      }
+    }
+    '''
+    r = requests.post(graphql_url, json={'query': query}, headers={'Authorization': f'Bearer {token}'})
+    return get_badges_by_username(username)
+
+
+def grant_badge_by_discord_id(badge_id, discord_id, token):
+    username = username_from_discord_id(discord_id)
+    return grant_badge_by_username(badge_id, username, token)
