f2ea678b53a8e47a9dfb0b2d94f610f76ab52be2
Lola <lola@egherman.com>
2021-04-01 10:39:48 -0500

    missed a line

--- src/utils/user.py
+++ src/utils/user.py
@@ -17,7 +17,7 @@ async def update_username(bot, user_info):
                 displayed_badges = [badge["details"]["emoji"] for badge in
                                     [badge_data for badge_data in user_info["badges"] if
                                      badge_data["displayed"] is True]]
-            desired_nick = f"{user_info['name']} {''.join(displayed_badges)}"
+            desired_nick = f"{random.choice(fish)} {user_info['name']} {''.join(displayed_badges)}"
         try:
             if not user.nick == desired_nick:
                 try:
